f06f88887526a7351ecb9cab1ddc13c85a57510a
Kacper <kacper2208@gmail.com>
2024-07-09 08:23:19 +0200

    implement post portfolio

--- api/Repository/PortfolioRepository.cs
+++ api/Repository/PortfolioRepository.cs
@@ -17,10 +17,19 @@ namespace api.Repository
         {
             _context = context;
         }
+
+        public async Task<Portfolio> CreateAsnyc(Portfolio portfolio)
+        {
+            await _context.Portfolios.AddAsync(portfolio);
+            await _context.SaveChangesAsync();
+            return portfolio;
+        }
+
         public async Task<List<Stock>> GetUserPortfolio(AppUser user)
         {
             return await _context.Portfolios.Where(u => u.AppUserId == user.Id)
-            .Select(stock => new Stock{
+            .Select(stock => new Stock
+            {
                 Id = stock.StockId,
                 Symbol = stock.Stock.Symbol,
                 CompanyName = stock.Stock.CompanyName,
